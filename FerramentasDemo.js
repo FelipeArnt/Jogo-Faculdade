4bad4580a16988847020a6c4d7c26ee03e3e71c0
Felipe Arnt <102182726+FelipeArnt@users.noreply.github.com>
2025-10-28 18:22:47 -0300

    Add files via upload

---
+++ FerramentasDemo.js
@@ -0,0 +1,21 @@
+import {Ferramenta} from "./Basicas.js";
+
+// ---------------------------------------------
+export class Chave extends Ferramenta {
+	constructor() {
+		super("chave");
+	}
+}
+// ---------------------------------------------
+export class Martelo extends Ferramenta {
+	constructor() {
+		super("martelo");
+	}
+}
+// ---------------------------------------------
+export class Notebook extends Ferramenta {
+  constructor(){
+    super("notebook");
+  }
+
+}
